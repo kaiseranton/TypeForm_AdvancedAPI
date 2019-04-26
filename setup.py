733ba7c5709fd72edb6e3671882ee72c5e33a436
Anton Kaiser <akaiser@twinschool.co.uk>
2019-04-26 15:27:57 +0100

    RELEASE

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="typeform_advancedapi",
+	install_requires=[
+          'requests>=2.21.0'
+      ],
+    version="1.0.6",
+    author='Anton Kaiser',
+    author_email='kaiser@kaiser.ovh',
+    description="TypeForm API written in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kaiseranton/TypeForm_AdvancedAPI",
+    packages=['typeform_advancedapi'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
